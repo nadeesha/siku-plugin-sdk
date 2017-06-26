247b6fe886d707027fe1871276c611d3bfafc80b
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2017-06-27 08:41:45 +1000

    Initial commit with types

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as _ from 'lodash';
+import * as moment from 'moment';
+
+export interface IUtils {
+    get: <TResponse>(url: string, headers: any) => Promise<TResponse>;
+    post: <TResponse>(url: string, headers: any, body: any) => Promise<TResponse>;
+    _: typeof _,
+    moment: typeof moment,
+}
+
+export interface IPluginConfig {
+    redirectUrl: string;
+    clientId: string;
+    clientSecret: string;
+}
+
+export interface IEvent {
+    id: string;
+    timestamp: number;
+    type: string;
+    multiplier: number;
+}
+
+export interface IPluginDefinition<PluginUserConfig extends {}> {
+    getAuthorizationUrl: () => string;
+    getAccessToken: <AuthResponse extends {}>(authResponse: AuthResponse) => Promise<string>;
+    editUserConfig: () => Promise<PluginUserConfig>;
+    getEvents: () => Promise<IEvent[]>;
+    eventTypes: {
+        [key:string]: {
+            description: string;
+        };
+    };
+}
+
+export type PluginGenerator = <PluginUserConfig>(utils: IUtils, pluginConfig: IPluginConfig, userConfig: PluginUserConfig) => IPluginDefinition<PluginUserConfig>;
