76ed002bfd6f071982c2f03f0f7daada94b6c3ce
Nadeesha Cabral <n@nadeesha.me>
2018-01-10 23:21:08 +1100

    Update authResponse types

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,39 @@ import * as _ from 'lodash';
 import * as moment from 'moment';
 
 export interface IUtils {
-    get: (url: string, headers: any) => Promise<Response>;
-    post: (url: string, headers: any, body: any) => Promise<Response>;
-    _: typeof _,
-    moment: typeof moment,
+  get: (url: string, headers: any) => Promise<Response>;
+  post: (url: string, headers: any, body: any) => Promise<Response>;
+  _: typeof _;
+  moment: typeof moment;
 }
 
 export interface IPluginConfig {
-    redirectUrl: string;
-    clientId: string;
-    clientSecret: string;
+  redirectUrl: string;
+  clientId: string;
+  clientSecret: string;
 }
 
 export interface IEvent {
-    id: string;
-    timestamp: number;
-    type: string;
-    multiplier: number;
+  id: string;
+  timestamp: number;
+  type: string;
+  multiplier: number;
 }
 
 export interface IPluginDefinition<PluginUserConfig extends {}> {
-    getAuthorizationUrl: () => Promise<string>;
-    getAccessToken: <AuthResponse extends {}>(authResponse: AuthResponse) => Promise<string>;
-    editUserConfig: () => Promise<PluginUserConfig>;
-    getEvents: () => Promise<IEvent[]>;
-    eventTypes: {
-        [key:string]: {
-            description: string;
-        };
+  getAuthorizationUrl: () => Promise<string>;
+  getAccessToken: (authResponse: any) => Promise<string>;
+  editUserConfig: () => Promise<PluginUserConfig>;
+  getEvents: () => Promise<IEvent[]>;
+  eventTypes: {
+    [key: string]: {
+      description: string;
     };
+  };
 }
 
-export type PluginGenerator<PluginUserConfig> = (utils: IUtils, pluginConfig: IPluginConfig, userConfig: PluginUserConfig) => IPluginDefinition<PluginUserConfig>;
+export type PluginGenerator<PluginUserConfig> = (
+  utils: IUtils,
+  pluginConfig: IPluginConfig,
+  userConfig: PluginUserConfig
+) => IPluginDefinition<PluginUserConfig>;
